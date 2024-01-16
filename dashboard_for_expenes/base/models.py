4b93b71cc14529e2af716f060f90ad10b666373f
Szabson123 <szymonzaba18@gmail.com>
2024-01-16 11:04:48 +0100

    Create superuser and add models

--- dashboard_for_expenes/base/models.py
+++ dashboard_for_expenes/base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Expense(models.Model):
+    name = models.CharField(max_length=255)
+    cost = models.DecimalField(max_digits=15, decimal_places=6)
+    descripton = models.TextField()
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    
+    def __str__(self):
+        return self.name
+    
