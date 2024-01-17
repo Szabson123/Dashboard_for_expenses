404fe8b2ab18f65523fd8e284a0d7cd8a61358d9
Szabson123 <szymonzaba18@gmail.com>
2024-01-17 12:53:19 +0100

    added dashborad model

--- dashboard_for_expenes/base/models.py
+++ dashboard_for_expenes/base/models.py
@@ -3,30 +3,42 @@ from datetime import datetime
 from django.contrib.auth.models import User
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.user.username
+
+
+class Dashboard(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    target = models.DecimalField(max_digits=30, decimal_places=2)
+    data = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 class Expense(models.Model):
     name = models.CharField(max_length=255)
     cost = models.DecimalField(max_digits=15, decimal_places=2)
     descripton = models.TextField()
     date = models.DateTimeField(default=datetime.now, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    dashboard = models.ForeignKey(Dashboard, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
     
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return self.user.username
-    
-    
+ 
 class Earnings(models.Model):
     name = models.CharField(max_length=255)
     money = models.DecimalField(max_digits=15, decimal_places=2)
     description = models.TextField(blank=True)
     date = models.DateTimeField(default=datetime.now, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    dashboard = models.ForeignKey(Dashboard, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
